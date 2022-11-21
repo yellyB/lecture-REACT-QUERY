4cc07418344dd143ec79f20cbbd6015ee7149e30
bomiKim <kk3p1q1@gmail.com>
2022-11-21 11:48:16 +0900

    8. React Query 개발자 도구

--- Resources/base-blog-em/src/App.jsx
+++ Resources/base-blog-em/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { Posts } from "./Posts";
 
 const queryClient = new QueryClient();
@@ -12,6 +13,7 @@ function App() {
         <h1>Blog Posts</h1>
         <Posts />
       </div>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
