84b40c157a977f030790258a6eb8cb1cc9ce4ec8
bomiKim <kk3p1q1@gmail.com>
2022-11-22 10:56:23 +0900

    Play 11. 코드 퀴즈! 블로그 댓글을 위한 쿼리 생성하기

--- Resources/base-blog-em/src/PostDetail.jsx
+++ Resources/base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,7 +25,17 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isError, error, isLoading } = useQuery("postDetail", () =>
+    fetchComments(post?.id)
+  );
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>oops, something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    ); // 세번 시도 후 에러로 판단
 
   return (
     <>
