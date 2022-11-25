18b88b05edc17cf9929058cf28fe4f2ec78b4d75
bomiKim <kk3p1q1@gmail.com>
2022-11-25 12:35:12 +0900

    Play 18. 코드 퀴즈! 변이(Mutation)로 포스팅 제목 업데이트하기

--- Resources/base-blog-em/src/PostDetail.jsx
+++ Resources/base-blog-em/src/PostDetail.jsx
@@ -31,6 +31,7 @@ export function PostDetail({ post }) {
   );
 
   const deleteMutation = useMutation((postId) => deletePost(postId));
+  const updateMutation = useMutation((postId) => updatePost(postId));
 
   if (isLoading) return <h3>Loading...</h3>;
   if (isError)
@@ -54,7 +55,18 @@ export function PostDetail({ post }) {
       {deleteMutation.isSuccess && (
         <p style={{ color: "green" }}>Post has (not) been deleted</p>
       )}
-      <button>Update title</button>
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+      {updateMutation.isError && (
+        <p style={{ color: "red" }}>Error updating the post</p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{ color: "purple" }}>Updating the post</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: "green" }}>Post has (not) been updated</p>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
