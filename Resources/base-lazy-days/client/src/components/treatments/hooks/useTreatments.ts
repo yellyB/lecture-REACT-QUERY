37e2dc89b7917846eac817d27d76c366792faa51
bomiKim <kk3p1q1@gmail.com>
2023-05-13 22:40:23 +0900

    41. useQuery에 대한 onError 핸들러

--- Resources/base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ Resources/base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -8,11 +8,22 @@ import { useCustomToast } from '../../app/hooks/useCustomToast';
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
   const { data } = await axiosInstance.get('/treatments');
+  // throw 'error';
   return data;
 }
 
 export function useTreatments(): Treatment[] {
+  const toast = useCustomToast();
+
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    onError: (error) => {
+      const title =
+        error instanceof Error
+          ? error.message
+          : 'error connection to the server';
+      toast({ title, status: 'error' });
+    },
+  });
   return data;
 }
