493519fba9241a0af1ecf3bb5582054f1deea2df
bomiKim <kk3p1q1@gmail.com>
2023-05-17 18:16:55 +0900

    48. 프리페칭(Pre-fetching) 처리 (구문)

--- Resources/base-lazy-days/client/src/components/app/Home.tsx
+++ Resources/base-lazy-days/client/src/components/app/Home.tsx
@@ -3,8 +3,18 @@ import { ReactElement } from 'react';
 import { GiFlowerPot } from 'react-icons/gi';
 
 import { BackgroundImage } from '../common/BackgroundImage';
+import { usePrefetchTreatments } from '../treatments/hooks/useTreatments';
 
 export function Home(): ReactElement {
+  /**
+   * Home 에서 usePrefetchTreatments 를 실행해도 괜찮은가?
+   *  -> 일단, Home은 정적 페이지라 괜찮을텐데. 그래도 불안하면
+   *  -> useEffect안에 넣어서 쓸까? => 훅 안에서 훅 사용 불가
+   *  -> usePrefetchTreatments를 훅이 아니게 만들까? => 훅이 아니면 훅 내에서 useQueryClient 실행 할 수 없음
+   *  -> staleTime, cacheTime을 옵션에 넣어서 관리하기
+   */
+  usePrefetchTreatments();
+
   return (
     <Stack align="center" justify="center" height="84vh">
       <BackgroundImage />
--- Resources/base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ Resources/base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
@@ -15,4 +15,11 @@ export function useTreatments(): Treatment[] {
   const fallback = [];
   const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
   return data;
+}
+
+export function usePrefetchTreatments(): void {
+  // 프리페칭하여 캐시에 넣을거임.
+  // -> treatments 불러와야 하기 전에. home 화면에서 미리 불러와서 캐싱하기
+  const queryClient = useQueryClient();
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
 }
